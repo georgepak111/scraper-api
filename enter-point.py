a12ab32cdf9250b1661f01621eca82974bba2bb2
georgepak111 <george.sedgwick@go-pakgroup.com>
2026-05-01 12:25:31 +0100

    Update enter-point.py

--- enter-point.py
+++ enter-point.py
@@ -3,29 +3,28 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-data = json.loads(sys.stdin.read())  # Read and parse the data from Node
+data = json.loads(sys.stdin.read())  # fixed the link issue
 url = data["url"]
-
-
-def clean_html(filename):
-    file = open(filename, "r")
-    soup = BeautifulSoup(file, "html.parser")
-    body_text = soup.body.get_text(separator="\n", strip=True)
-    return(body_text)
 
 def get_html(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
-        html = response.text
-        return html
+        return response.text
     except requests.exceptions.RequestException:
-        return "BAD REQUEST"
+        return None
+
+def clean_html(html_string):  # now takes a string, not a filename
+    soup = BeautifulSoup(html_string, "html.parser")
+    body_text = soup.body.get_text(separator="\n", strip=True)
+    return body_text
 
 def main(url):
-  all_html = get_html(url)
-  cleaned = clean_html(all_html)
-  print(cleaned)
-
+    html = get_html(url)
+    if html is None:
+        print("BAD REQUEST")
+        return
+    cleaned = clean_html(html)
+    print(cleaned)
 
 main(url)
